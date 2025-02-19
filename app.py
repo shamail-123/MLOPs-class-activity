f76234a71257eac9b959705e6735a6894de91ce2
Shamail_Aamir <shamailkhan757@gmail.com>
2025-02-19 21:46:37 +0500

    app.py update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "This is Shamail Aamir Khan "
+    return "This is Shamail Aamir Khan 21I-1772  "
 
 if __name__ == '__main__':
     app.run()
